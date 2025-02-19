d74753a06295703a906574baeef3e7aaf0dfb6fc
4canzz <156192043+4canzz@users.noreply.github.com>
2025-02-19 09:59:38 +0500

    Update Problem-4.cpp

--- LAB-4/Problem-4.cpp
+++ LAB-4/Problem-4.cpp
@@ -1,1 +1,54 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+	int n;
+	cout << "Enter the size of the matrix (n x n): ";
+	cin >> n;
+
+	int** matrix = new int*[n];
+	for (int i = 0; i < n; i++) {
+		matrix[i] = new int[n];
+	}
+
+	cout << "Enter elements of the matrix:\n";
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> matrix[i][j];
+		}
+	}
+	cout << "Original Matrix:\n";
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
+	int** transpose = new int*[n];
+	for (int i = 0; i < n; i++) {
+		transpose[i] = new int[n];
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			transpose[j][i] = matrix[i][j];
+		}
+	}
+	cout << "Transposed Matrix:\n";
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << transpose[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i < n; i++) {
+		delete[] matrix[i];
+		delete[] transpose[i];
+	}
+
+	delete[] matrix;
+	delete[] transpose;
+
+	return 0;
+}
