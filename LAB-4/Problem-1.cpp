f289cd0e9f6c0cd8618d40ee22d58eaaf91efff3
4canzz <156192043+4canzz@users.noreply.github.com>
2025-02-19 09:17:08 +0500

    Update Problem-1.cpp

--- LAB-4/Problem-1.cpp
+++ LAB-4/Problem-1.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+	int rows, cols;
+	cout << "Enter rows: " << endl;
+	cin >> rows;
+	cout << "Enter columns: " << endl;
+	cin >> cols;
+
+	int** arr = new int*[rows];
+	for (int i = 0; i < rows; i++) {
+		arr[i] = new int[cols];
+	}
+
+	cout << "Enter elements row wise: " << endl;
+	for (int i = 0; i < rows; i++){
+		for (int j = 0; j < cols; j++){
+			cin >> arr[i][j];
+		}
+	}
+
+	cout << "2D Array: " << endl;
+	for (int i = 0; i < rows; i++){
+		for (int j = 0; j < cols; j++){
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i<rows; i++){
+		delete[] arr[i];
+	}
+
+	delete[] arr;
+	system("pause");
+	return 0;
+}
