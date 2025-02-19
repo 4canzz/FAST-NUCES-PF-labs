5c7af2c389d576a012df22e0ec90ddf5017bfa09
4canzz <156192043+4canzz@users.noreply.github.com>
2025-02-19 09:56:23 +0500

    Update Problem-3.cpp

--- LAB-4/Problem-3.cpp
+++ LAB-4/Problem-3.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+	int rows, cols;
+	cout << "Enter rows for 2D Array: " << endl;
+	cin >> rows;
+	cout << "Enter columns for 2D Array: " << endl;
+	cin >> cols;
+
+	int** arr = new int*[rows];
+	for (int i = 0; i < rows; i++) {
+		arr[i] = new int[cols];
+	}
+
+	cout << "Enter elements row wise: " << endl;
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			cin >> arr[i][j];
+		}
+	}
+
+	cout << "Minimum values in each row: " << endl;
+	for (int i = 0; i < rows; i++) {
+		int min = arr[i][0];
+		for (int j = 0; j < cols; j++) {
+			if (arr[i][j] < min) min = arr[i][j];
+		}
+		cout << min << endl;
+	}
+
+	for (int i = 0; i<rows; i++) {
+		delete[] arr[i];
+	}
+
+	delete[] arr;
+
+	system("pause");
+	return 0;
+}
